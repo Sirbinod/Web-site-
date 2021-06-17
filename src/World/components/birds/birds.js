d3c206a5ad15f22a9a3d29a5ef6a577b8b023d62
Sarbin Shrestha <sarbin@supremeit.net>
2021-06-17 20:56:17 +0545

    loader manage

--- src/World/components/birds/birds.js
+++ src/World/components/birds/birds.js
@@ -6,16 +6,31 @@ async function loadBirds() {
   const loader = new GLTFLoader();
 
   const [parrotData] = await Promise.all([
+    // image one
     loader.loadAsync("/assets/models/Parrot.glb"),
+
+    // image two
+    // loader.loadAsync("/assets/models/Flamingo.glb"),
+
+    // image third
+    // loader.loadAsync("/assets/models/Stork.glb"),
   ]);
 
   console.log("Squaaawk!", parrotData);
 
   const parrot = setupModel(parrotData);
-  parrot.position.set(0, 0, 2.5);
+  parrot.position.set(-1.5, 1, 2.5);
+
+  // const flamingo = setupModel(flamingoData);
+  // flamingo.position.set(7.5, 0, -10);
+
+  // const stork = setupModel(storkData);
+  // stork.position.set(0, -2.5, -10);
 
   return {
     parrot,
+    // flamingo,
+    // stork,
   };
 }
 
